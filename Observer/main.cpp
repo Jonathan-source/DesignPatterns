1e3e8a11cd1265bbc9a61d9ad797e9c2af59735a
Jonathan <53872055+Jonathan-source@users.noreply.github.com>
2022-06-07 19:42:37 +0200

    Create main.cpp

---
+++ Observer/main.cpp
@@ -0,0 +1,106 @@
+/*
+    In this demo, I have applied the observer design pattern to allow
+    one-to many dependency between objects. This pattern is modeled on
+    the event-driven programming paradigm and lets us define loosely 
+    coupled systems. Whenever an observable object changes its state, 
+    its corresponding observer classes are notified and, only then,
+    executes their behaviours.
+
+    Updated: 2022-06-07
+    Author: Jonathan Helsing [github.com/Jonathan-source]
+*/
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+//#include <windows.h>
+//#include <mmsystem.h>
+
+#include "IObserver.h"
+#include "ISubject.h"
+#include "Events.h"
+
+
+// Player is the subject of observation.
+class Player : public ISubject<Player> {
+
+public:
+
+    Player() : m_experience(0) {}
+    virtual ~Player() = default;
+
+    void KillCritter(int amount = 1)
+    {
+        for (int i = 0; i < amount; ++i)
+        {
+            std::cout << "Player: I've killed a critter!" << std::endl;
+            m_experience += 10;
+            NotifyObservers(*this, Event::CRITTER_KILLED);
+        }
+    }
+
+    int m_experience;
+
+};
+
+
+// ScoreBoard observes the Player.
+class ScoreBoard : public IObserver<Player> {
+
+public:
+
+    ScoreBoard() : m_crittersKilled(0) {}
+    virtual ~ScoreBoard() = default;
+
+    NotifyAction OnNotify(Player& subject, const Event& event) override
+    {
+        if (event == Event::CRITTER_KILLED)
+        {
+            std::cout << "ScoreBoard: A total of " + std::to_string(++m_crittersKilled) +
+                " have been killed by the player, who has a total of " + std::to_string(subject.m_experience) +
+                " experience" << std::endl;
+        }
+            
+        return NotifyAction::Done;
+    }
+
+    int m_crittersKilled;
+
+};
+
+
+// AudioManager observes the Player.
+class AudioManager : public IObserver<Player> {
+
+public:
+
+    AudioManager() = default;
+    virtual ~AudioManager() = default;
+
+    NotifyAction OnNotify(Player& subject, const Event& event) override
+    {
+        if (event == Event::CRITTER_KILLED)
+        {
+            // TODO: make a safer version using mutex.
+            std::cout << R"(PlaySoundW(TEXT("critter_killed.wav"), NULL, SND_FILENAME | SND_ASYNC);)" << std::endl;
+        }
+
+        return NotifyAction::Done;
+    }
+};
+
+
+int main()
+{   
+    Player player;
+    ScoreBoard scoreBoard;
+    AudioManager audioManager;
+
+    player.RegisterObserver(scoreBoard);
+    player.RegisterObserver(audioManager);
+
+    player.KillCritter(5);
+
+    return 0;
+}
